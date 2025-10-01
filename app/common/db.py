3597ab501e2538dae9904b543dfee93144519fae
JuicyS8da <alzaharov20@gmail.com>
2025-10-01 20:52:42 +0700

    Deploy

--- app/common/db.py
+++ app/common/db.py
@@ -5,15 +5,9 @@ from sqlalchemy.orm import declarative_base
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "music_db")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-
-DATABASE_URL = (
-    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
